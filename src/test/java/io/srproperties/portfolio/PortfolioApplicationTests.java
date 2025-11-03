9cda07a9309635ebf7856182ac0c7c2ece9d8470
Siva <prasadareddy21@gmail.com>
2025-11-02 18:38:56 -0600

    Initial commit

---
+++ src/test/java/io/srproperties/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.srproperties.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
